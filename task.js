93fbc25ef34c4a59c8730b216af4733372c7510c
anelka ganteng <alanelka42@gmail.com>
2022-12-07 09:07:04 +0700

    initial commit

--- task.js
+++ task.js
@@ -12,19 +12,8 @@ const showDownload = (result) => {
  * @param {function} callback - Function callback show
  */
 const download = async (callShowDownload) => {
-  //   setTimeout(function () {
-  //     const result = "windows-10.exe";
-  //     callShowDownload(result);
-  //   }, 3000);
   const result = "windows-10.exe";
   await callShowDownload(result);
 };
 
 download(showDownload);
-
-/**
- * TODO:
- * - Refactor callback ke Promise atau Async Await
- * - Refactor function ke ES6 Arrow Function
- * - Refactor string ke ES6 Template Literals
- */
